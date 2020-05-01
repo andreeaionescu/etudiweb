97f272155662ef45edb4bbd0202b9d87d543ace2
Andreea Ionescu <amionescu15@gmail.com>
2020-05-01 19:55:10 +0100

    Added material ui core. Created costum theme.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,63 @@
 import React from 'react';
+import { makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#c1eff4',
+      main: '#b2ebf2',
+      dark: '#7ca4a9',
+      contrastText: '#000',
+    },
+    secondary: {
+      light: '#aa90d7',
+      main: '#9575cd',
+      dark: '#68518f',
+      contrastText: '#fff',
+    },
+  }
+});
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function App() {
+
+  const classes = useStyles();
+
   return (
     <div style={{flexGrow:1, height:'100%'}}>
+       <ThemeProvider theme={theme}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Etudi
+            </Typography>
+            <Button color="inherit">Home</Button>
+            <Button color="inherit">About</Button>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+        </ThemeProvider>
     </div>
   );
 }
