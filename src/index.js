a7ed9c5c441b57bf47a4f44c0f9501dfb3fce61a
Andreea Ionescu <amionescu15@gmail.com>
2020-06-27 14:04:43 +0100

    Added aws-amplify config.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
-
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
 ReactDOM.render(
     <Provider store={store}>
